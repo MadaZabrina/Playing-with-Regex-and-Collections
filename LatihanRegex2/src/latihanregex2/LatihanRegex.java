2d5c462e6bbd702930db89384aa52e14760b60ff
MadaZabrina <61772782+MadaZabrina@users.noreply.github.com>
2020-03-19 20:40:00 +0700

    Add files via upload

---
+++ LatihanRegex2/src/latihanregex2/LatihanRegex.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanregex2;
+
+/**
+ *
+ * @author ZABRINA
+ */
+public class LatihanRegex {
+   
+    
+    public static void main(String[]args){
+    MainFrame frame = new MainFrame();
+    frame.setVisible(true);
+    }
+}
---
+++ LatihanRegex2/src/latihanregex2/Logic.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanregex2;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author ZABRINA
+ */
+public class Logic {
+    public Boolean checkEmail(String email){
+        Pattern pattern = Pattern.compile("^([a-zA-Z0-9\\-\\.]+)@([a-zA-Z0-9-"
+                + "\\-\\.]+)\\.([a-zA-Z]{2,5})$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    
+    public ArrayList addData(ArrayList list, String data){
+        list.add(data);
+        
+        return list;
+    }
+    
+}
